fcff869423ad36a6f06d7d43db234ee7dadf844f
Abhishek Mishra <abhishek.mishra@w3villa.com>
2021-12-22 09:29:08 +0530

    Added login system and session manually

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id  #session obj. by rails
+ 			flash[:notice] = "Successfully logged in"
+			redirect_to user
+		else
+			flash.now[:notice] = "Wrong entries, please retry"			
+			render 'new'
+
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash[:notice] = "logged out Successfully"
+		redirect_to root_path
+ 	end
+
+end
